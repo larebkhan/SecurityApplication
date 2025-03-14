b53e160021f11400da750c5e01cdc5882f27013d
Lareb <lareb131213@gmail.com>
2025-03-14 12:10:27 +0530

    Upgraded the jwt authentication to store refresh token

--- src/main/java/com/codingShuttle/SecurityApp/SecurityApplication/SecurityApplication.java
+++ src/main/java/com/codingShuttle/SecurityApp/SecurityApplication/SecurityApplication.java
@@ -1,10 +1,20 @@
 package com.codingShuttle.SecurityApp.SecurityApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SecurityApplication {
+
+	static {
+		Dotenv dotenv = Dotenv.configure()
+				.directory("src/main/resources") // Specify the directory
+				.filename("secrets.env")         // Load this file
+				.load();;
+		System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+		System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
--- src/test/java/com/codingShuttle/SecurityApp/SecurityApplication/SecurityApplicationTests.java
+++ src/test/java/com/codingShuttle/SecurityApp/SecurityApplication/SecurityApplicationTests.java
@@ -13,14 +13,14 @@ class SecurityApplicationTests {
 	@Autowired
 	private JwtService jwtService;
 
-	@Test
-	void contextLoads() {
-		User user = new User(4L, "lareb131231@gmail.com","1234");
-		String token = jwtService.generateToken(user);
-		System.out.println(token);
-		Long id = jwtService.getUserIdFromToken(token);
-		System.out.println(id);
-
-	}
+//	@Test
+//	void contextLoads() {
+//		User user = new User(4L, "lareb131231@gmail.com","1234");
+//		String token = jwtService.generateToken(user);
+//		System.out.println(token);
+//		Long id = jwtService.getUserIdFromToken(token);
+//		System.out.println(id);
+//
+//	}
 
 }
